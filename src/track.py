c0f905e50a0258894b85870d4bcc8742cea69670
Skyler Carlson <skylercarlson28@gmail.com>
2020-11-28 13:01:43 -0700

    Add show method

--- src/track.py
+++ src/track.py
@@ -2,6 +2,8 @@
 
 import re
 import numpy as np
+import sys
+from copy import deepcopy
 
 class Track():
     """Class for racetrack.
@@ -12,19 +14,20 @@ class Track():
         Dimensions of track
 
     track : np.array
-        Numpy array representation of track
+        Array representation of track
 
     start : tuple
-        Tuple containing (rows, cols) of starting line
+        Tuple containing (row, col) of starting line (midpoint is used)
 
-    finish : 
+    finish : tuple
+        Tuple containing finish line
 
     Methods
     -------
 
     """
 
-    def __init__(self, filename, verbose=False):
+    def __init__(self, filename):
         """Initializes a Track object. Assumes the track file will
         be in `data/` directory.
 
@@ -32,14 +35,9 @@ class Track():
         ----------
         filename : string
             Filename in data/ directory
-
-        verbose : bool
-            Verbosity switch
-
         """
 
         self.filename = filename
-        self.verbose = verbose
 
         # Read the file
         filename = 'data/' + filename
@@ -52,13 +50,11 @@ class Track():
         dims = track_split[0].split(',')
         self.dims = [int(i) for i in dims]
 
-        # Convert to numpy array
         self.track = track_split[1:-1]
 
         # Get starting line and finish line
         self.start = self.get_start_line(self.track)
         self.finish = self.get_finish_line(self.track)
-
 
     def get_start_line(self, track):
         """Finds the starting line of the track. Uses the
@@ -76,7 +72,7 @@ class Track():
         """
 
         row_list = [idx for idx, ele in enumerate(track) if 'S' in ele]
-    
+
         if len(row_list) == 1:
             row = row_list[0]
             col_tuple = re.search('S+', track[row]).span()
@@ -104,7 +100,7 @@ class Track():
         """
 
         row_list = [idx for idx, ele in enumerate(track) if 'F' in ele]
-    
+
         if len(row_list) == 1:
             row = row_list[0]
             col_tuple = re.search('F+', track[row]).span()
@@ -116,3 +112,35 @@ class Track():
 
         return row, col
 
+    def show(self, car=None):
+        """Shows the track with the car (optional)
+
+        Parameters
+        ----------
+        car : tuple, optional
+            Location of car (row, col), by default None
+        """
+
+        sys.stdout.flush()
+
+        if car is not None:
+            # Check that the location is valid
+            assert type(car) == tuple, 'Location must be a tuple'
+            assert len(car) == 2, 'Car location length invalid'
+
+            track_car = deepcopy(self.track)
+
+            # Extract the row to a list and assign
+            row = list(track_car[car[0]])
+            row[car[1]] = 'X'
+
+            # Delete the old row
+            del track_car[car[0]]
+
+            # Insert the new row
+            row_str = ''.join(row)
+            track_car.insert(car[0], row_str)
+
+            [print(row) for row in track_car]
+        else:
+            [print(row) for row in self.track]
