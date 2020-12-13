8c4087b3771485daf61b7ce313d676ea704b3140
Skyler Carlson <skylercarlson28@gmail.com>
2020-12-12 19:37:14 -0700

    Add race file

---
+++ race.py
@@ -0,0 +1,14 @@
+from src.track import Track
+from src.q_learn import QLearning
+import numpy as np
+
+
+# Create track and race objects (doesn't matter that it's a Q-learning object
+# because racing functionality is the same for all)
+track = Track('L-track.txt')
+race = QLearning(track, gamma=0.5, eta=None)
+
+# Race with the specified policy
+filename = 'L-track_test_Q_1mil.npy'
+policy = np.load(filename)
+race.race(policy=policy)
